ae4cb6389d44f8215520d92959fdae80555b58d2
TristanVarewijck <tristanconnect4@outlook.com>
2021-02-09 16:57:37 +0100

    a filter route added

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
+app.get('/filter', (req, res) => {
+    res.send('Filter the groups your looking for!')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
