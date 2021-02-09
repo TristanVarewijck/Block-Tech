03de7de78a9548d21816d1b8b9fc830236a4e92e
TristanVarewijck <tristanconnect4@outlook.com>
2021-02-09 17:12:20 +0100

    404 route added

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-
-
 // Met de require() method kan je javascript modules in laden. 
 
 const express = require('express');
 const app = express();
 const port = 3000;
 
+
+// Pagina's 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
@@ -22,6 +22,12 @@ app.get('/filter', (req, res) => {
     res.send('Filter the groups your looking for!')
 });
 
+// 404 pagina 
+app.use(function (req, res, next) {
+    res.status(404).send("404 error")
+})
+
+// server 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
