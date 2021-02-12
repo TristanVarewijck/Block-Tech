1f2922d64bbc8b293340e8930bae8fe9a41682d5
TristanVarewijck <tristanconnect4@outlook.com>
2021-02-12 11:52:10 +0100

    pug first try

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
 // Met de require() method kan je javascript modules in laden. 
-
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 
-// statische pagina's 
-app.use(express.static('public')) 
-
-// Pagina's 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
-
-app.get('/login', (req, res) => {
-    res.send('Login into your account')
-});
-
-app.get('/about', (req, res) => {
-    res.send('The about page')
-});
-
-
-// 404 pagina 
-app.use(function (req, res, next) {
-    res.status(404).send("404 error")
+router.get("/", function(req, res) {
+    res.send('index');
 })
 
 
+// statische pagina's 
+// app.use(express.static('public')) 
+
+// Pagina's 
+// app.get('/', (req, res) => {
+ // res.send('Hello World!')
+// });
+
+// 404 pagina 
+// app.use(function (req, res, next) {
+ //   res.status(404).send("404 error")
+//})
+
+
 // server 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
-});
+//app.listen(port, () => {
+ // console.log(`Example app listening on port ${port}!`)
+//});
 
 
 
