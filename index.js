50e8871da6fbf6571912680e19510ae99a411612
TristanVarewijck <tristanconnect4@outlook.com>
2021-03-09 12:55:12 +0100

    heroku

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 const { MongoClient } = require('mongodb')
 // Mongo connection
 let db = null
-console.log(process.env.DB_URI)
+console.log(`process.env.DB_URI ${process.env.DB_URI}`)
 // funciton connectDB
 async function connectDB () {
   // get URL from .env file
