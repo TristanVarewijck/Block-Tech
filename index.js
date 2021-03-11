9d869ac53cc0bfdda547f58d5d271f3237bf188d
TristanVarewijck <tristanconnect4@outlook.com>
2021-03-11 10:40:10 +0100

    heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 const { 
   MongoClient 
 } = require('mongodb');
